bcb7105475177f4a63a93000acda22b8302d7e51
swrnvinod40 <swrnvinod40@gmail.com>
2022-02-11 15:07:48 -0600

    connection

--- src/handlers/ExpressHandler.js
+++ src/handlers/ExpressHandler.js
@@ -3,6 +3,9 @@ const app = express()
 const port = 3000
 const Get = require('../services/Get');
 const MongooseConnection = require('../configuration/MongooseConnection')
+const _ = require('lodash');
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -15,8 +18,23 @@ app.get('/categories', async (req, res) => {
     return res;
 })  
   
-app.listen(port, () => {
+app.get('/recentlyAddedProducts', async (req, res)=>{
+    let get = new Get();
+    console.log('printing recenly added products');
+    res.send("yes");
+    return res;
+});
+
+app.post('/productDetailsByOrderId',jsonParser, async (req, res) =>{
+  let get = new Get();
+  let payload = _.isObject(req) ? req : JSON.parse(req);
+  console.log('Printing productDetailsByOrderId', payload.body);
+  res.send('NO');
+  return res;
+});
+
+app.listen(port, async () => {
     let mongooseConnection = new MongooseConnection();
-    mongooseConnection.initializeConnection()
+    await mongooseConnection.initializeConnection()
       console.log(`Example app listening at http://localhost:${port}`)
 })
